6af064179afb062e9a53786b0033e95e461176b5
QiaoPengjun5162 <qiaopengjun0@gmail.com>
2024-10-24 17:58:18 +0800

    Finish conversions exercises by implementing TryFrom related methods, remove I AM NOT DONE comments, and refactor code to use related methods for tuple, array, and slice implementations.

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -27,8 +27,6 @@ enum IntoColorError {
     IntConversion,
 }
 
-// I AM NOT DONE
-
 // Your task is to complete this implementation and return an Ok result of inner
 // type Color. You need to create an implementation for a tuple of three
 // integers, an array of three integers, and a slice of integers.
@@ -37,10 +35,24 @@ enum IntoColorError {
 // time, but the slice implementation needs to check the slice length! Also note
 // that correct RGB color values must be integers in the 0..=255 range.
 
+// 帮助函数：检查值是否在 0..=255 之间
+fn check_value(value: i16) -> Result<u8, IntoColorError> {
+    if value >= 0 && value <= 255 {
+        Ok(value as u8)
+    } else {
+        Err(IntoColorError::IntConversion)
+    }
+}
+
 // Tuple implementation
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        Ok(Color {
+            red: check_value(tuple.0)?,
+            green: check_value(tuple.1)?,
+            blue: check_value(tuple.2)?,
+        })
     }
 }
 
@@ -48,6 +60,11 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        Ok(Color {
+            red: check_value(arr[0])?,
+            green: check_value(arr[1])?,
+            blue: check_value(arr[2])?,
+        })
     }
 }
 
@@ -55,6 +72,14 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            return Err(IntoColorError::BadLen);
+        }
+        Ok(Color {
+            red: check_value(slice[0])?,
+            green: check_value(slice[1])?,
+            blue: check_value(slice[2])?,
+        })
     }
 }
 
